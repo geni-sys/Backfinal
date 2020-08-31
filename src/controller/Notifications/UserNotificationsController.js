6519eb5603d2d6ea9998c97f731f6bd0a922daf9
Elias alexandre <elliasallex@gmail.com>
2020-08-30 21:40:00 -0300

    Feat> NotificAtions flts

--- src/controller/Notifications/UserNotificationsController.js
+++ src/controller/Notifications/UserNotificationsController.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-console */
 /* eslint-disable camelcase */
+const {
+  Op,
+} = require('sequelize');
 const User = require('../../model/User');
 const Notifications = require('../../model/Notifications');
 
@@ -9,6 +12,10 @@ module.exports = {
     const {
       receiver,
     } = request.params;
+    const {
+      mentioned,
+      our_team,
+    } = request.query;
 
     try {
       const snd = await User.findByPk(receiver);
@@ -18,7 +25,55 @@ module.exports = {
         });
       }
 
-      const notification = await Notifications.findAll({
+      let notification = [];
+
+      if (mentioned) {
+        notification = await Notifications.findAll({
+          attributes: {
+            exclude: ['createdAt'],
+          },
+          where: {
+            receiver,
+            type: {
+              [Op.like]: '%mention%',
+            },
+          },
+          include: [{
+            association: 'de',
+            attributes: ['id', 'name', 'email'],
+          }, {
+            association: 'para',
+            attributes: ['id', 'name', 'email'],
+          }],
+        });
+
+        return response.json(notification);
+      }
+
+      if (our_team) {
+        notification = await Notifications.findAll({
+          attributes: {
+            exclude: ['createdAt'],
+          },
+          where: {
+            receiver,
+            type: {
+              [Op.like]: '%ourteam%',
+            },
+          },
+          include: [{
+            association: 'de',
+            attributes: ['id', 'name', 'email'],
+          }, {
+            association: 'para',
+            attributes: ['id', 'name', 'email'],
+          }],
+        });
+
+        return response.json(notification);
+      }
+
+      notification = await Notifications.findAll({
         attributes: {
           exclude: ['createdAt'],
         },
